441dfa644afa5ad1e29cb48493a7357c7edbfa37
TrailBlazers04 <sushil.goud@lntinfotech.com>
2020-10-07 12:57:39 +0530

    Angular Css

--- OnlineExam/src/app/exam/exam.component.ts
+++ OnlineExam/src/app/exam/exam.component.ts
@@ -12,7 +12,37 @@ export class ExamComponent implements OnInit {
   top : string = "";
   topics = [ "JAVA", "SQL", "JQUERY"];
 
-  list : Quizquestion[] = [];
+  list : Quizquestion[] = [
+  ];
+
+  allQuestions: any = [{
+		"id": 1,
+		"question": "What is the capital of Belgium?",
+		"a": "Vienna",
+		"b": "Berlin",
+		"c": "Brussels",
+		"d": "Prague",
+		"answer": "c"
+	},
+	{
+		"id": 2,
+		"question": "What is the capital of Australia?",
+		"a": "Vienna",
+		"b": "Canberra",
+		"c": "Brussels",
+		"d": "Prague",
+		"answer": "b"
+	},
+	{
+		"id": 3,
+		"question": "What is the capital of Bulgaria?",
+		"a": "Vienna",
+		"b": "Sofia",
+		"c": "Brussels",
+		"d": "Prague",
+		"answer": "b"
+	}
+	];
 
   constructor(private _service : QuizserviceService) { }
 
@@ -28,4 +58,7 @@ export class ExamComponent implements OnInit {
       error => console.log("exception occured")
     );
   }
+
+
+
 }
